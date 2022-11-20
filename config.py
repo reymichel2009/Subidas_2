00f6764d904e3e1b6ae0ec6ba50925ebcf464af9
labestia2080 <100966021+reymichel2009@users.noreply.github.com>
2022-11-20 01:33:41 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,11 @@ API_HASH = os.environ.get('api_hash','d1a5974070430293b64fde5339591447')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','reymichel2009').split(';')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFDDKDYEJIJFGFYDJEGKGJYEIJIIRDGELGGJKG'))
+static_proxy = '' #agrega si kieres tener un proxy statico Con @raydel0307 si kieres comprar un proxy
+PROXY = ProxyCloud.parse(static_proxy)
 
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
+
+space = {}
+space['reymichel2009', 'george0x0'] = 0
